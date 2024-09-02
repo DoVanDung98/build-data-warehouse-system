cfe7a2369951ddf0abbc6f2bf86c25fc61982724
dungdv34 <dungdv34@xanhsm.com>
2024-09-02 17:02:23 +0700

    add sale order key to fact sales order line table

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -7,6 +7,7 @@ WITH fact_sales_order_line__source AS(
 , fact_sales_order_line__rename AS(
   SELECT
     order_line_id AS sales_order_line_key
+    , order_id AS sales_order_key
     , stock_item_id AS product_key
     , quantity AS quantity
     , unit_price AS unit_price
@@ -16,6 +17,7 @@ WITH fact_sales_order_line__source AS(
 , fact_sales_order_line__cast_type AS(
   SELECT
     CAST(sales_order_line_key AS INTEGER) AS sales_order_line_key
+    , CAST(sales_order_key AS INTEGER) AS sales_order_key
     , CAST(product_key AS INTEGER) AS product_key
     , CAST(quantity AS INTEGER) AS quantity
     , CAST(unit_price AS INTEGER) AS unit_price
