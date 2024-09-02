ba219e6e54bb4080fdf876d211837c71f53c79dc
dungdv34 <dungdv34@xanhsm.com>
2024-09-02 16:08:23 +0700

    add cast data type

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -1,5 +1,5 @@
 SELECT 
-  stock_item_id as product_key
-  , stock_item_name as product_name
-  , brand as brand_name
+  CAST(stock_item_id as INTEGER) as product_key
+  , CAST(stock_item_name as STRING) as product_name
+  , CAST(brand as STRING) as brand_name
 FROM `vit-lam-data.wide_world_importers.warehouse__stock_items`
--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -1,7 +1,7 @@
 SELECT
-  order_line_id as sales_order_line_key
-  , stock_item_id as product_key
-  , quantity
-  , unit_price
-  , quantity * unit_price as gross_amount
+  CAST(order_line_id as INTEGER) as sales_order_line_key
+  , CAST(stock_item_id as INTEGER) as product_key
+  , CAST(quantity as INTEGER) as quantity
+  , CAST(unit_price as NUMERIC) as unit_price
+  , CAST(quantity as INTEGER) * CAST(unit_price as INTEGER) as gross_amount
 FROM `vit-lam-data.wide_world_importers.sales__order_lines` 
