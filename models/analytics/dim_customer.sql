38852524c33fedf53ca08734b6df90e620eae00e
dungdv34 <dungdv34@xanhsm.com>
2024-09-02 16:41:17 +0700

    add dim customer column

--- models/analytics/dim_customer.sql
+++ models/analytics/dim_customer.sql
@@ -1,3 +1,21 @@
-SELECT 
-  *
-FROM `vit-lam-data.wide_world_importers.sales__customers`
+WITH dim_customer__source AS(
+  SELECT 
+    *
+  FROM `vit-lam-data.wide_world_importers.sales__customers`
+)
+
+, dim_customer__rename AS(
+  SELECT
+    customer_id AS customer_key
+    , customer_name AS customer_name
+  FROM dim_customer__source
+)
+
+, dim_customer_cast_type AS(
+  SELECT
+    CAST(customer_key AS INTEGER) AS customer_key
+    , CAST(customer_name AS STRING) AS customer_name
+  FROM dim_customer__rename
+)
+
+SELECT * FROM dim_customer_cast_type
