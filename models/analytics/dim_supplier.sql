7606f14a88ab155260e191161e0b4b698c8c5a8a
dungdv34 <dungdv34@xanhsm.com>
2024-09-03 13:44:46 +0700

    create a dim supplier table

--- models/analytics/dim_supplier.sql
+++ models/analytics/dim_supplier.sql
@@ -1,3 +1,21 @@
-SELECT 
-  *
-FROM `vit-lam-data.wide_world_importers.purchasing__suppliers`
+WITH dim_supplider__source AS(
+  SELECT 
+    *
+  FROM `vit-lam-data.wide_world_importers.purchasing__suppliers`
+)
+
+, dim_supplider__rename AS(
+  SELECT
+    supplier_id AS supplier_key
+    , supplier_name AS supplier_name
+  FROM dim_supplider__source
+)
+
+, dim_supplider__cast_type AS(
+  SELECT 
+    CAST(supplier_key AS INTEGER) AS supplier_key
+    , CAST(supplier_name AS STRING) AS supplier_name
+  FROM dim_supplider__rename
+)
+
+SELECT * FROM dim_supplider__cast_type
