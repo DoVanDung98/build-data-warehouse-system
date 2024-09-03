6049d9f98370801b32424c5cc76564cdceea66cf
dungdv34 <dungdv34@xanhsm.com>
2024-09-03 13:59:03 +0700

    flatten dim_supplier table to dim_product table

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -9,6 +9,7 @@ WITH dim_product__source AS(
     stock_item_id as product_key
     , stock_item_name as product_name
     , brand as brand_name
+    , supplier_id as supplier_key
   FROM dim_product__source
 )
 
@@ -17,6 +18,15 @@ WITH dim_product__source AS(
     CAST(product_key AS INTEGER) as product_key
     , CAST(product_name AS STRING) as product_name
     , CAST(brand_name AS STRING) as brand_name
+    , CAST(supplier_key AS INTEGER) as supplier_key
   FROM dim_product__rename_column
 )
-SELECT * FROM dim_product__cast_type
+SELECT
+  dim_product.product_key
+  , dim_product.product_name
+  , dim_product.brand_name
+  , dim_product.supplier_key
+  , dim_supplier.supplier_name
+FROM dim_product__cast_type AS dim_product
+LEFT JOIN {{ ref('dim_supplier') }} AS dim_supplier
+ON dim_product.supplier_key=dim_supplier.supplier_key
