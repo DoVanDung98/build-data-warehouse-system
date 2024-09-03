ce695d55010bea4f18d343f6d003db9128d0c159
dungdv34 <dungdv34@xanhsm.com>
2024-09-03 15:46:06 +0700

    flatten customer category and buying group to customer table

--- models/analytics/dim_customer.sql
+++ models/analytics/dim_customer.sql
@@ -23,12 +23,12 @@ WITH dim_customer__source AS(
 )
 
 SELECT 
-  customer_key
-  , customer_name
-  , customer_category_key
-  , customer_category_name
-  , buying_group_key
-  , buying_group_name
-FROM dim_customer_cast_type
-LEFT JOIN {{ ref('stg_dim_buying_group') }} USING(buying_group_key)
-LEFT JOIN {{ ref('stg_dim_customer_category')}} USING(customer_category_key)
+  dim_customer.customer_key
+  , dim_customer.customer_name
+  , dim_customer.customer_category_key
+  , dim_customer_category.customer_category_name
+  , dim_customer.buying_group_key
+  , dim_buying_group.buying_group_name
+FROM dim_customer_cast_type AS dim_customer
+LEFT JOIN {{ ref('stg_dim_customer_category')}} AS dim_customer_category USING(customer_category_key) 
+LEFT JOIN {{ ref('stg_dim_buying_group') }} AS dim_buying_group USING(buying_group_key) 
